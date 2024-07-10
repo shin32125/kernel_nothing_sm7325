ecefcff865b8bfaed66a986a3bb1990eb9a28d65
aswin7469 <aswinas@pixysos.com>
2024-07-10 21:17:19 +0530

    treewide: fix build after tag merges
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- mm/mlock.c
+++ mm/mlock.c
@@ -728,7 +728,7 @@ SYSCALL_DEFINE3(mlock2, unsigned long, start, size_t, len, int, flags)
 	vm_flags_t vm_flags = VM_LOCKED;
 
 	if (flags & ~MLOCK_ONFAULT)
-		return -EINAL;
+		return -EINVAL;
 
 	if (flags & MLOCK_ONFAULT)
 		vm_flags |= VM_LOCKONFAULT;
